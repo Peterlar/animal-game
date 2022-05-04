2053806c0afccdf24a1a4dd362f06a6fda23ea05
Peterlar <Peter_kjaer@hotmail.com>
2022-05-04 13:57:05 +0000

    Added function for selecting winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ selectionButtons.forEach(selectionButton => {
       makeSelection(selection)
     })
   })
+
+  function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+  
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+  
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
+  }
